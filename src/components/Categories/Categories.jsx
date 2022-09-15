f01eae088bb03a112f0af5b6274752302e6b5478
Federico Luna <fluna1973@yahoo.com>
2022-09-15 17:53:40 -0600

    wireframe set up

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => (
+  <div>
+    <button type="button" className="statusBtn">Check status</button>
+  </div>
+);
+
+export default Categories;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+
+const NavBar = () => (
+  <div className="containerNavBar">
+    <div className="NavBar">
+      <div>
+        <h1>Bookstore CMS</h1>
+      </div>
+      <div className="links">
+        <Link to="/Books" className="books">Books</Link>
+        <Link to="/Categories" className="categories">Catagories</Link>
+      </div>
+    </div>
+  </div>
+);
+
+export default NavBar;
